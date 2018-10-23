ef715c9a6794df3dfbe5ceb6461c4537b94b0269
mazoorek <piotr.mazurek98@gmail.com>
2018-10-23 23:07:11 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/tv_shows_console_app.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tv_shows_console_app.dir/main.cpp.obj"
+  "tv_shows_console_app.pdb"
+  "tv_shows_console_app.exe"
+  "tv_shows_console_app.exe.manifest"
+  "libtv_shows_console_app.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tv_shows_console_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/tv_shows_console_app.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tv_shows_console_app.
+# This may be replaced when dependencies are built.
